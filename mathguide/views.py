68eee9db088c649657f54ef7018233d32bd85492
LHY-42 <LHY-42@users.noreply.github.com>
2022-03-20 17:28:14 +0800

    h

---
+++ mathguide/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.urls import reverse
+from django.http import  HttpResponseRedirect, Http404
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from .models import User, Post
+from django.conf import settings
+from django import forms
+from django.forms import ModelForm, formset_factory, modelformset_factory
+
+# Create your views here.
+def index(request):
+
+
+    return render(request, "mathguide/index.html")
+
